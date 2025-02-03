513858b3c7ee4a49a89b53497bdb8caf8529dcdc
Allen Uy <109072233+uyallen@users.noreply.github.com>
2025-02-03 21:58:10 +0800

    my portfolio init commit

---
+++ my-portfolio/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { PrimeReactProvider } from 'primereact/api'
+
+import Portfolio from "./components/portfolio/Portfolio"
+
+function App() {
+  return (
+    <PrimeReactProvider>
+      <Portfolio />
+    </PrimeReactProvider>
+  );
+}
+
+export default App;
